76de3b1281355208a5d13ac2f1810efa2bda9356
Ruben Lopes <rubenssouss@hotmail.com>
2021-03-26 15:09:41 -0300

    Programa que inverte triangulo retangulo de base 5

---
+++ trybe-exercises/exercises/exercises/4.3/triRetAsteriscosInvert.js
@@ -0,0 +1,16 @@
+let triBase = 5;
+let baseArray = [];
+
+for(let index2 = 0; index2 < triBase; index2 += 1) {
+  
+  baseArray.push(' ');
+}
+
+for(let index = baseArray.length-1; index >= 0; index -= 1) {
+
+  baseArray[index] = '*';
+
+  console.log(baseArray.join(""));
+
+}
+
