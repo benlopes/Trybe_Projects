40443e8af3d5c8bbb49c5bd87586d9ef49cf446b
Ruben Lopes <rubenssouss@hotmail.com>
2021-03-29 15:48:27 -0300

    Exercise 1 - program that checks if word is a palindrome

---
+++ trybe-exercises/exercises/exercises/4.4/functions/ex_1_checkPalind.js
@@ -0,0 +1,17 @@
+unction isPal(word) {
+	
+	let isPal = false;
+	
+	// transf. str to arr; reverse the arr; transf. back to str
+	// transf. string (S) p/ array (A); reverte A; volta pra S
+	let arrWord = ((word.split('')).reverse()).join('');
+
+	if (word === arrWord) {
+		isPal = true;
+	}
+	return isPal;
+}
+
+/*console.log('arara', isPal('arara'));
+console.log('desenvolvimento', isPal('desenvolvimento'));
+console.log('ovo', isPal('ovo'));*/
